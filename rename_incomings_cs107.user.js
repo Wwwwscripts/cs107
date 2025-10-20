63220c2dfcfecfa811cb7f72702f155378d5a43a
Wwww <krizto.as@icloud.com>
2025-10-20 18:07:49 +0100

    Create rename_incomings_cs107.user.js

---
+++ rename_incomings_cs107.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Rename Incomings - cs107
+// @author       Wwww
+// @version      2.0
+// @updateURL    https://raw.githubusercontent.com/Wwwwscripts/cs107/main/rename_incomings_cs107.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Wwwwscripts/cs107/main/rename_incomings_cs107.user.js
+// @match        https://*/game.php?*&mode=incomings*
+// @icon         https://raw.githubusercontent.com/Wwwwscripts/share/refs/heads/main/W.png
+// @grant        none
+// ==/UserScript==
