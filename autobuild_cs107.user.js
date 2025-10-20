af056f93608a813198a01e8f17791f409dc33154
Wwww <krizto.as@icloud.com>
2025-10-20 18:03:20 +0100

    Create autobuild_cs107.user.js

---
+++ autobuild_cs107.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Auto Builder with Rewards - cs107
+// @version      2.0
+// @author       Wwww
+// @updateURL    https://raw.githubusercontent.com/Wwwwscripts/cs107/main/autobuild_cs107.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Wwwwscripts/cs107/refs/heads/main/autobuild_cs107.user.js
+// @match        https://*/game.php?village=*&screen=main*
+// @icon         https://raw.githubusercontent.com/Wwwwscripts/share/refs/heads/main/W.png
+// @grant        none
+// ==/UserScript==
