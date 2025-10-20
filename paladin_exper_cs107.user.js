3f696dc57245e309456453d754a606a69093dca6
Wwww <krizto.as@icloud.com>
2025-10-20 18:05:45 +0100

    Create paladin_exper_cs107.user.js

---
+++ paladin_exper_cs107.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Paladin Exper - cs107
+// @author      Wwww
+// @version      2.0
+// @updateURL    https://raw.githubusercontent.com/Wwwwscripts/cs107/main/paladin_exper_cs107.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Wwwwscripts/cs107/main/paladin_exper_cs107.user.js
+// @include      https://*/game.php?village=*&screen=statue&knight*
+// @icon         https://raw.githubusercontent.com/Wwwwscripts/share/refs/heads/main/W.png
+// @grant        none
+// ==/UserScript==
