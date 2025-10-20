e13f6903dddc11a46b049d320a33c6289c6c1d50
Wwww <krizto.as@icloud.com>
2025-10-20 18:02:36 +0100

    Create autobalancer_cs107.user.js

---
+++ autobalancer_cs107.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Auto Balancer
+// @author       Wwww
+// @version      2.0
+// @updateURL    https://raw.githubusercontent.com/Wwwwscripts/cs107/main/autobalancer_cs107.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Wwwwscripts/cs107/main/autobalancer_cs107.user.js
+// @match        https://*/*screen=storage
+// @icon         https://raw.githubusercontent.com/Wwwwscripts/share/refs/heads/main/W.png
+// @run-at       document-ready
+// @grant        none
+// ==/UserScript==
