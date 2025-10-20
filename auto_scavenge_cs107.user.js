c9d2da9b7992dfd4eab1ba808ad184a7405278c5
Wwww <krizto.as@icloud.com>
2025-10-20 18:01:21 +0100

    Create auto_scavenge_cs107.user.js

---
+++ auto_scavenge_cs107.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Auto Scavenge (withou premium, Spears only) - cs107
+// @author       Wwww
+// @version      2.0
+// @updateURL    https://raw.githubusercontent.com/Wwwwscripts/cs107/main/auto_scavenge_cs107.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Wwwwscripts/cs107/main/auto_scavenge_cs107.user.js
+// @match        https://*/game.php?*screen=place&*mode=scavenge
+// @match        https://*/game.php?*screen=place&*mode=scavenge&
+// @icon         https://raw.githubusercontent.com/Wwwwscripts/share/refs/heads/main/W.png
+// @grant        none
+// ==/UserScript==
