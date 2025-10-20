bfe06cb8d644fd9db15329fd5641c22eb06f053b
Wwww <krizto.as@icloud.com>
2025-10-20 18:08:19 +0100

    Update kumin_farm_cs107.user.js

--- kumin_farm_cs107.user.js
+++ kumin_farm_cs107.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Kumin Farm
+// @name         Kumin Farm - cs107
 // @author       GOAT
 // @version      2.0
 // @updateURL    https://raw.githubusercontent.com/Wwwwscripts/cs107/main/kumin_farm_cs107.meta.js
