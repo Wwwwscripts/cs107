665c9fbdea355d3955550d91bf9cec01619f39ae
Wwww <krizto.as@icloud.com>
2025-10-20 18:04:21 +0100

    Create kumin_farm_cs107.user.js

---
+++ kumin_farm_cs107.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Kumin Farm
+// @author       GOAT
+// @version      2.0
+// @updateURL    https://raw.githubusercontent.com/Wwwwscripts/cs107/main/kumin_farm_cs107.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Wwwwscripts/cs107/main/kumin_farm_cs107.user.js
+// @icon         https://raw.githubusercontent.com/Wwwwscripts/share/refs/heads/main/W.png
+// @match        https://*/*screen=am_farm*
+// @grant        none
+// ==/UserScript==
