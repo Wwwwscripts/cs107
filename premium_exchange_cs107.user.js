c51e8b52cc3fecd94c365dbd343b2834363ace6a
Wwww <krizto.as@icloud.com>
2025-10-20 18:06:23 +0100

    Create premium_exchange_cs107.user.js

---
+++ premium_exchange_cs107.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Premium Exchange - cs107
+// @author       Wwww
+// @version      2.1
+// @updateURL    https://raw.githubusercontent.com/Wwwwscripts/cs107/main/premium_exchange_cs107.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Wwwwscripts/cs107/main/premium_exchange_cs107.user.js
+// @include      https://*/game.php*market&mode=exchange*
+// @icon         https://raw.githubusercontent.com/Wwwwscripts/share/refs/heads/main/W.png
+// @run-at       document-ready
+// @grant        none
+// ==/UserScript==
