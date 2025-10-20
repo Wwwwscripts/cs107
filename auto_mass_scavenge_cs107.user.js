2c3b4e4cbe06cb8efd3a0f475bc60682f6a8b9f0
Wwww <krizto.as@icloud.com>
2025-10-20 17:56:44 +0100

    Create auto_mass_scavenge_cs107.user.js

---
+++ auto_mass_scavenge_cs107.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Auto Mass Scavenge
+// @author       Wwww
+// @version      2.0
+// @updateURL    https://raw.githubusercontent.com/Wwwwscripts/cs107/raw/refs/heads/main/auto_mass_scavenge_cs107.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Wwwwscripts/cs107/raw/refs/heads/main/auto_mass_scavenge_cs107.user.js
+// @match        https://*/*screen=place&mode=scavenge_mass*
+// @icon         https://raw.githubusercontent.com/Wwwwscripts/share/refs/heads/main/W.png
+// @run-at       document-ready
+// @grant        none
+// ==/UserScript==
