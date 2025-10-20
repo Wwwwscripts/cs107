fcb0c096a7f00b4949589e8d5ca4259619476191
Wwww <krizto.as@icloud.com>
2025-10-20 17:55:26 +0100

    Update auto_command_sender_cs107.user.js

--- auto_command_sender_cs107.user.js
+++ auto_command_sender_cs107.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Auto Command sender - cs107
 // @author       GOAT
-// @version      2.0
-// @updateURL    https://raw.githubusercontent.com/Wwwwscripts/share/main/auto_command_sender.meta.js
-// @downloadURL  https://raw.githubusercontent.com/Wwwwscripts/share/main/auto_command_sender.user.js
+// @version      1.0
+// @updateURL    https://raw.githubusercontent.com/Wwwwscripts/cs107/raw/refs/heads/main/auto_command_sender_cs107.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Wwwwscripts/cs107/raw/refs/heads/main/auto_command_sender_cs107.user.js
 // @include      *:/*/*
 // @exclude      https://*/game.php?*&screen=place&mode=scavenge_mass
 // @icon         https://raw.githubusercontent.com/Wwwwscripts/share/refs/heads/main/W.png
